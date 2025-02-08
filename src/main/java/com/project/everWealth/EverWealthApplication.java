3820e60ed3ba808d6742790e77e95c106fa0165c
abdulllkhan <abdulskhan47@gmail.com>
2025-02-08 14:14:55 -0500

    Base Setup

---
+++ src/main/java/com/project/everWealth/EverWealthApplication.java
@@ -0,0 +1,13 @@
+package com.project.everWealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverWealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverWealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/everWealth/EverWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.everWealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverWealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
